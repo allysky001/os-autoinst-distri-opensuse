9376f6d8bbf8126cdcc91a8cab277171ea2031c2
alice <xlai@suse.com>
2016-06-13 14:36:05 +0800

    virt_autotest: the initial version of virtualization automation test in openqa, with kvm support fully, xen support not done yet

---
+++ tests/virt_autotest/Prj2_Host_Upgrade/host_upgrade_step1_run.pm
@@ -0,0 +1,43 @@
+# SUSE's openQA tests
+#
+# Copyright © 2012-2016 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+#
+use lib "/var/lib/openqa/share/tests/sle-12-SP2/tests/virt_autotest/lib";
+use lib "/var/lib/openqa/share/tests/sle-12-SP2/tests/virt_autotest/Prj2_Host_Upgrade/";
+use base "host_upgrade_base";
+use testapi;
+use strict;
+
+sub get_scrip_run() {
+    my $self = shift;
+
+    my $pre_test_cmd = $self->get_test_name_prefix;
+    $pre_test_cmd .= "-run 01";
+    return "$pre_test_cmd";
+}
+
+sub run() { 
+    my $self = shift;
+    # Got script run according to different kind of system
+    my $pre_test_cmd = $self->get_scrip_run();
+
+    # Execute script run
+    my $ret = $self->execute_script_run($pre_test_cmd, 5400);
+    save_screenshot;
+
+    assert_script_run("tar cvf /tmp/host-upgrade-updateVirtRpms-logs.tar /var/log/qa/ctcs2/; rm  /var/log/qa/ctcs2/* -r", 60);
+
+    upload_logs("/tmp/host-upgrade-updateVirtRpms-logs.tar");
+
+    if ($ret !~ /Test run completed successfully/) {
+        die "Update virt rpms failed!";
+    }
+}
+
+1;
+
