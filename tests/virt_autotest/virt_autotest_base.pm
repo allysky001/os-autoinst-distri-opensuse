563de5fcbfb4b67d4f4c58273d3051dbd78da42b
alice <xlai@suse.com>
2016-07-07 17:06:21 +0800

    fix compilation errors

--- tests/virt_autotest/virt_autotest_base.pm
+++ tests/virt_autotest/virt_autotest_base.pm
@@ -121,11 +121,11 @@ sub run_test() {
     my $test_cmd = $self->get_script_run();
     my $script_output = $self->execute_script_run($test_cmd, $timeout);
 
-	if ("$add_junit_log_flag" = "yes") {
+	if ("$add_junit_log_flag" == "yes") {
 		$self->add_junit_log($script_output);
 	}
 
-	if ("$upload_virt_log_flag" = "yes") {
+	if ("$upload_virt_log_flag" == "yes") {
 		upload_virt_logs($log_dir, $compressed_log_name);
 	}
 
@@ -151,7 +151,7 @@ sub add_junit_log() {
 sub upload_virt_logs() {
 	my ($log_dir, $compressed_log_name) = @_;
 
-	my $full_compressed_log_name=/tmp/$compressed_log_name.tar
+	my $full_compressed_log_name = "/tmp/$compressed_log_name.tar";
     script_run("tar cvf $full_compressed_log_name $log_dir; gzip $full_compressed_log_name; rm $log_dir -r", 60);
     upload_logs "$full_compressed_log_name.gz";
 
